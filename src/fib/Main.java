addf5c23661340a814576fbd87c0c61b477a4ca7
Do Trinh <thanhdo.trinh@gmail.com>
2022-12-21 20:21:57 +0900

    show Fibonacci SEQ - Recursion

--- src/fib/Main.java
+++ src/fib/Main.java
@@ -6,23 +6,40 @@ public class Main {
     public static void main(String[] args) {
         Main m = new Main();
 //        m.using_for();
-        m.using_while();
+//        m.using_while();
+        m.using_recur();
     }
 
+    public void using_recur() {
+        int N = 10;
+        // Print the first N numbers
+        for (int i = 0; i < N; i++) {
+            LogI(fib(i) + " ");
+        }
+    }
+
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+
     public void using_while() {
-         int n1 = 0;
-         int n2 = 1;
-         int n3 = 0;
-         int count = 0;
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = 0;
+        int count = 0;
         LogI("fib: " + n1);
-         while (count < 4) {
-             n3 = n1 + n2;
-             LogI("fib: " + n2);
-             LogI("fib: " + n3);
-             n1 = n3;
-             n2 = n3 + n2;
-             count++;
-         }
+        while (count < 4) {
+            n3 = n1 + n2;
+            LogI("fib: " + n2);
+            LogI("fib: " + n3);
+            n1 = n3;
+            n2 = n3 + n2;
+            count++;
+        }
     }
 
     public void using_for() {
