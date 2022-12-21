48a24da4c83b8b38164d7d6914bde77d2831c4a8
Do Trinh <thanhdo.trinh@gmail.com>
2022-12-21 20:02:23 +0900

    show Fibonacci SEQ - for

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.using_for();
+    }
+
+    public void using_for() {
+        int n1 = 0, n2 = 1, n3, i, count = 23;
+        LogI("fib: " + n1);
+        LogI("fib: " + n2);
+        for (i = 2; i < count; ++i) {
+            n3 = n1 + n2;
+            LogI("fib: " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+
+    public static void LogI(String Message) {
+        System.out.println("viewlog " + Message);
+    }
+
+}
