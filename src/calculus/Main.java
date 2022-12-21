de12448c666e8aee3fb60c063d8b97d2653ee0ec
Do Trinh <thanhdo.trinh@gmail.com>
2022-12-21 20:07:38 +0900

    reorganize

---
+++ src/calculus/Main.java
@@ -0,0 +1,16 @@
+package calculus;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main m = new Main();
+
+        LogI("hello");
+    }
+
+    public static void LogI(String Message) {
+        System.out.println("viewlog " + Message);
+    }
+
+}
--- src/fib/Main.java
+++ src/fib/Main.java
@@ -1,4 +1,4 @@
-
+package fib;
 
 
 public class Main {
